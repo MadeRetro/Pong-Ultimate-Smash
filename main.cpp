a0e7d7041706a2a029d000b95d120bcec96b80cf
Rémy CHOLLET <153066606+MadeRetro@users.noreply.github.com>
2024-02-15 17:53:44 +0100

    [update]
    Menu + Score + Super Game + WAIDH

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+enum Scene {
+    MENU,
+    GAME,
+    SCORE
+};
+
 float screenHeight;
 float screenWidth;
 
@@ -12,7 +18,8 @@ Vector2 ballSpeed;
 
 Vector2 leftPaddlePosition;
 Vector2 rightPaddlePosition;
-Vector2 titleposition{ 290,20 };
+
+Vector2 titlePosition{ 275,20 };
 
 const int paddleWidth = 10;
 const int paddleHeight = 80;
@@ -20,6 +27,8 @@ const int paddleSpeed = 5;
 
 int leftPlayerScore = 0;
 int rightPlayerScore = 0;
+
+Scene currentScene = MENU;
 
 
 
@@ -31,8 +40,8 @@ void ResetBall() {
     float angle = GetRandomValue(45, 50);
 
     // Calculate the corresponding x and y components of the speed
-    ballSpeed.x = cos(angle * DEG2RAD) * 5; // 5 is the speed, adjust as needed
-    ballSpeed.y = sin(angle * DEG2RAD) * 5;
+    ballSpeed.x = cos(angle * DEG2RAD) * 10; // 5 is the speed, adjust as needed
+    ballSpeed.y = sin(angle * DEG2RAD) * 10;
 }
 
 
@@ -60,49 +69,186 @@ int main() {
 
 
     while (!WindowShouldClose()) {
-        // Update
-        ballPosition.x += ballSpeed.x;
-        ballPosition.y += ballSpeed.y;
 
-        // Bounce off the walls
-        if ((ballPosition.y + ballRadius > screenHeight) || (ballPosition.y - ballRadius < 0)) {
-            ballSpeed.y = -ballSpeed.y;
+
+        switch (currentScene) {
+        case MENU:
+            // Draw menu
+            BeginDrawing();
+            ClearBackground(DARKBLUE);
+            DrawTextEx(ft, "Pong Ultimate", titlePosition, 30, 15, WHITE);
+            DrawText("Press any key to start", (screenWidth / 4)+100, screenHeight / 2, 20, WHITE);
+            EndDrawing();
+
+            // Check for key press to start the game
+            if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_ESCAPE) || IsKeyPressed(KEY_M)) {
+
+                leftPlayerScore = 0;
+                rightPlayerScore = 0;
+
+                currentScene = GAME;
+            }
+            break;
+
+
+        case GAME:
+
+
+
+            // Update
+            ballPosition.x += ballSpeed.x;
+            ballPosition.y += ballSpeed.y;
+
+            // Bounce off the walls
+            if ((ballPosition.y + ballRadius > screenHeight) || (ballPosition.y - ballRadius < 0)) {
+                ballSpeed.y = -ballSpeed.y;
+            }
+
+            // Bounce off the paddles
+            if (CheckCollisionCircleRec(ballPosition, ballRadius, { leftPaddlePosition.x, leftPaddlePosition.y, static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }) ||
+                CheckCollisionCircleRec(ballPosition, ballRadius, { rightPaddlePosition.x, rightPaddlePosition.y, static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) })) {
+                ballSpeed.x = -ballSpeed.x;
+            }
+
+            // Score points
+            if (ballPosition.x - ballRadius < 0) {
+                // Right player scores a point
+                rightPlayerScore++;
+                ResetBall();
+            }
+            else if (ballPosition.x + ballRadius > screenWidth) {
+                // Left player scores a point
+                leftPlayerScore++;
+                ResetBall();
+            }
+
+
+            // Control paddles
+            if (IsKeyDown(KEY_W) && leftPaddlePosition.y > 0) {
+                leftPaddlePosition.y -= paddleSpeed;
+            }
+            if (IsKeyDown(KEY_S) && (leftPaddlePosition.y + paddleHeight) < screenHeight) {
+                leftPaddlePosition.y += paddleSpeed;
+            }
+
+            if (IsKeyDown(KEY_UP) && rightPaddlePosition.y > 0) {
+                rightPaddlePosition.y -= paddleSpeed;
+            }
+            if (IsKeyDown(KEY_DOWN) && (rightPaddlePosition.y + paddleHeight) < screenHeight) {
+                rightPaddlePosition.y += paddleSpeed;
+            }
+
+
+            // Draw
+
+            BeginDrawing();
+            ClearBackground(DARKBLUE);
+
+            // Draw ball
+            DrawCircleV(ballPosition, ballRadius, WHITE);
+
+
+
+            // Draw paddles
+            DrawRectangleV(leftPaddlePosition, { static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }, WHITE);
+            DrawRectangleV(rightPaddlePosition, { static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }, WHITE);
+
+
+            // Draw text
+
+            //DrawTextEx(ft, "Pong Ultimate", titlePosition, 30, 10, WHITE);
+
+
+            //Previous text code idea
+            //DrawText("Pong        Ultimate", 325,10, 30,WHITE);
+
+
+            // Drawing three lines to make them thicker
+
+            for (int i = 0; i < screenHeight; i += 15) {
+                DrawLineV({ static_cast<float>(screenWidth / 2), static_cast<float>(i) },
+                    { static_cast<float>(screenWidth / 2), static_cast<float>(i + 10) }, WHITE);
+            }
+
+            for (int i = 0; i < screenHeight; i += 15) {
+                DrawLineV({ static_cast<float>((screenWidth / 2) + 1), static_cast<float>(i) },
+                    { static_cast<float>((screenWidth / 2) + 1), static_cast<float>(i + 10) }, WHITE);
+            }
+
+            for (int i = 0; i < screenHeight; i += 15) {
+                DrawLineV({ static_cast<float>((screenWidth / 2) - 1), static_cast<float>(i) },
+                    { static_cast<float>((screenWidth / 2) - 1), static_cast<float>(i + 10) }, WHITE);
+            }
+
+
+            // Draw scores
+            DrawTextEx(ft, TextFormat("%d", leftPlayerScore), { screenWidth / 4, 10 }, 30, 2, WHITE);
+            DrawTextEx(ft, TextFormat("%d", rightPlayerScore), { screenWidth * 3 / 4 - MeasureText(TextFormat("%d", rightPlayerScore), 30), 10 }, 30, 2, WHITE);
+
+
+            EndDrawing();
+
+            // Check for key press to start the game
+            if (IsKeyPressed(KEY_ESCAPE) || IsKeyPressed(KEY_P)) {
+                currentScene = MENU;
+            }
+
+
+
+            if (leftPlayerScore == 10 || rightPlayerScore == 10) {
+                currentScene = SCORE;
+            }
+
+            break;
+
+
+        case SCORE:
+            // ... (Score screen logic will be added here)
+
+
+                        // Draw menu
+            BeginDrawing();
+            ClearBackground(DARKBLUE);
+            DrawTextEx(ft, "Pong Ultimate", titlePosition, 30, 15, WHITE);
+
+
+            if (leftPlayerScore == 10) {
+
+                DrawText("Left", (screenWidth / 4) + 50, screenHeight / 2, 20, WHITE);
+
+            }
+
+            if (rightPlayerScore == 10) {
+
+                DrawText("Right", (screenWidth / 4) + 40, screenHeight / 2, 20, WHITE);
+
+            }
+
+
+            DrawText("player wins ! Congrats !", (screenWidth / 4) + 100, screenHeight / 2, 20, WHITE);
+            EndDrawing();
+
+            // Check if the player wants to replay the game
+            if (IsKeyPressed(KEY_SPACE)) {
+
+                leftPlayerScore = 0;
+                rightPlayerScore = 0;
+
+                currentScene = GAME;
+            }
+
+            // Check if the player wants to go back to the menu
+            if (IsKeyPressed(KEY_P)) {
+
+                currentScene = MENU;
+            }
+
+
+
+
+            break;
         }
 
-        // Bounce off the paddles
-        if (CheckCollisionCircleRec(ballPosition, ballRadius, { leftPaddlePosition.x, leftPaddlePosition.y, static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }) ||
-            CheckCollisionCircleRec(ballPosition, ballRadius, { rightPaddlePosition.x, rightPaddlePosition.y, static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) })) {
-            ballSpeed.x = -ballSpeed.x;
-        }
-
-        // Score points
-        if (ballPosition.x - ballRadius < 0) {
-            // Right player scores a point
-            rightPlayerScore++;
-            ResetBall();
-        }
-        else if (ballPosition.x + ballRadius > screenWidth) {
-            // Left player scores a point
-            leftPlayerScore++;
-            ResetBall();
-        }
-
-
-
-        // Control paddles
-        if (IsKeyDown(KEY_W) && leftPaddlePosition.y > 0) {
-            leftPaddlePosition.y -= paddleSpeed;
-        }
-        if (IsKeyDown(KEY_S) && (leftPaddlePosition.y + paddleHeight) < screenHeight) {
-            leftPaddlePosition.y += paddleSpeed;
-        }
-
-        if (IsKeyDown(KEY_UP) && rightPaddlePosition.y > 0) {
-            rightPaddlePosition.y -= paddleSpeed;
-        }
-        if (IsKeyDown(KEY_DOWN) && (rightPaddlePosition.y + paddleHeight) < screenHeight) {
-            rightPaddlePosition.y += paddleSpeed;
-        }
 
 
 
@@ -112,58 +258,13 @@ int main() {
 
 
 
-
-
-        // Draw
-
-        BeginDrawing();
-        ClearBackground(DARKBLUE);
-
-        // Draw ball
-        DrawCircleV(ballPosition, ballRadius, WHITE);
-
-
-
-        // Draw paddles
-        DrawRectangleV(leftPaddlePosition, { static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }, WHITE);
-        DrawRectangleV(rightPaddlePosition, { static_cast<float>(paddleWidth), static_cast<float>(paddleHeight) }, WHITE);
-
-
-        // Draw text
-
-        DrawTextEx(ft, "Pong Ultimate", titleposition , 30, 20, WHITE);
-        //DrawText("Pong        Ultimate", 325,10, 30,WHITE);
-        
-
-        // Drawing three lines to make them thicker
-
-        for (int i = 0; i < screenHeight; i += 15) {
-            DrawLineV({ static_cast<float>(screenWidth / 2), static_cast<float>(i) },
-                { static_cast<float>(screenWidth / 2), static_cast<float>(i + 10) }, WHITE);
-        }
-
-        for (int i = 0; i < screenHeight; i += 15) {
-            DrawLineV({ static_cast<float>((screenWidth / 2)+1), static_cast<float>(i) },
-                { static_cast<float>((screenWidth / 2)+1), static_cast<float>(i + 10) }, WHITE);
-        }
-
-        for (int i = 0; i < screenHeight; i += 15) {
-            DrawLineV({ static_cast<float>((screenWidth / 2) - 1), static_cast<float>(i) },
-                { static_cast<float>((screenWidth / 2) - 1), static_cast<float>(i + 10) }, WHITE);
-        }
-
-
-        // Draw scores
-        DrawTextEx(ft, TextFormat("%d", leftPlayerScore), { screenWidth / 4, 10 }, 30,2, WHITE);
-        DrawTextEx(ft, TextFormat("%d", rightPlayerScore), { screenWidth * 3 / 4 - MeasureText(TextFormat("%d", rightPlayerScore), 30), 10 }, 30,2, WHITE);
-
-
-        EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
+//
 
 
 
